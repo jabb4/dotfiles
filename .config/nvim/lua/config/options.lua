eca9eb20363be9af2e0cea521053f458b7161e74
Jacob Bengtsson <87302129+jabb4@users.noreply.github.com>
2026-05-26 22:22:04 +0200

    feat(nvim): add modular lazy.nvim config
    LSP via mason, treesitter, telescope, blink.cmp completion, and format-on-save.

---
+++ .config/nvim/lua/config/options.lua
@@ -0,0 +1,26 @@
+local opt = vim.opt
+
+opt.number = true
+opt.relativenumber = true
+opt.cursorline = true
+opt.signcolumn = "yes"
+opt.scrolloff = 8
+opt.wrap = false
+
+opt.expandtab = true
+opt.shiftwidth = 2
+opt.tabstop = 2
+opt.smartindent = true
+
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.termguicolors = true
+opt.mouse = "a"
+opt.clipboard = "unnamedplus"
+opt.undofile = true
+opt.updatetime = 250
+opt.timeoutlen = 400
---
+++ .config/nvim/lua/plugins/editor.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    opts = {},
+  },
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "stevearc/oil.nvim",
+    lazy = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
+    },
+    opts = { view_options = { show_hidden = true } },
+  },
+}
